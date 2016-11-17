b65ded512742e2e1cf11c08b789c759b845cb103
eef2118 <esoto@lavca.org>
2016-11-16 22:06:23 -0500

    Commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 
 	$('.button2').on('click', function(){
-      $('.button2').text('Thank you!');
+      $('.button2').text('Thank you! Check your email to confirm.');
 
       });
 
@@ -190,6 +190,32 @@ $(window).on("scroll", function(){
 );  
     }
   });
+
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+$(btn).on("click", function() {
+    modal.style.display = "block";
+});
+
+// When the user clicks on <span> (x), close the modal
+$(span).on("click", function() {
+    modal.style.display = "none";
+});
+
+// When the user clicks anywhere outside of the modal, close it
+$(window).on("click", function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }  
+});
+
 
 
 
